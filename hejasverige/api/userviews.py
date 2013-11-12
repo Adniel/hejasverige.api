490a638a14ca9de80173b4c77c2b0e690ed5e987
Daniel Grindelid <daniel.grindelid@gmail.com>
2013-11-12 14:31:48 +0100

    Updated returned club with club object and added invited members

--- hejasverige/api/userviews.py
+++ hejasverige/api/userviews.py
@@ -3,6 +3,7 @@
 from Products.CMFCore.interfaces import ISiteRoot
 from five import grok
 import json
+import datetime
 from plone import api
 from Products.CMFCore.utils import getToolByName
 from plone.app.uuid.utils import uuidToObject
@@ -12,6 +13,8 @@ from hejasverige.content.eventsubscribers import _getFolder
 
 import logging
 logger = logging.getLogger(__name__)
+
+
 
 class GetMembers(grok.View):
     """ Lists all available recipients and corresponding associasions
@@ -23,7 +26,7 @@ class GetMembers(grok.View):
 
     def get_club(self, id):
         catalog = getToolByName(self.context, 'portal_catalog')
-        return [dict(name=club.Title, vat_no=club.personal_id, uid=club.UID) for club in
+        return [dict(name=club.Title, vat_no=club.personal_id, uid=club.UID, club=club) for club in
                 catalog({'object_provides': IClub.__identifier__,
                          'personal_id': id,
                          })]
@@ -101,15 +104,53 @@ class GetMembers(grok.View):
 
         return relations
 
+    def get_invited(self, club):
+        data = []
+        catalog = getToolByName(self.context, 'portal_catalog')
+
+        from hejasverige.invitation.interfaces import IInvitation
+
+        query = {}
+        query['object_provides'] = IInvitation.__identifier__
+        query['path'] = dict(query=club.getPath())
+        query['review_state'] = 'pending'
+
+        invitations = catalog(query)
+
+        for invitation in invitations:
+            if invitation.ExpirationDate > datetime.datetime.now():
+                o = invitation.getObject()
+                rec = {}
+                rec['type'] = 'invited'
+                rec['personal_id'] = o.personal_id
+                rec['fullname'] = o.first_name + ' ' + o.last_name
+                rec['email'] = o.recipient_email
+                rec['personal_id'] = o.personal_id
+                rec['status'] = invitation.review_state
+                data.append(rec)
+        
+        return data
+
     def render(self):
         data = []
         if self.request['QUERY_STRING']:
-            #import pdb; pdb.set_trace()
             vat_no = self.request.form.get('vat_no', None)
+            invited = self.request.form.get('invited', None)
             club = self.get_club(vat_no)
             relations = self.get_club_relations(vat_no)
-            #data.append(dict(q=self.request['QUERY_STRING'], id=id, club=club, relations=relations))
             data = relations
+
+            if invited == 'true':
+                personal_ids = [v['personal_id'] for v in data]
+                invited = self.get_invited(club[0].get('club'))
+
+                for i in invited:
+                    if i.get('personal_id') not in personal_ids:
+                        data.append(i)
+                        personal_ids.append(i.get('personal_id'))
+                    else:
+                        logger.info('Did not include %s from invited. Already a present member.' % (i.get('personal_id'),))
+
         self.request.response.setHeader('Content-Type', 'application/json')
         return json.dumps(data, indent=4)
 
@@ -174,7 +215,7 @@ class GetMember(grok.View):
             # possibly a person
             # handle that
             # (there might appear cases where a user is also a person guarded by another user, we do not
-            #  handle that yet)
+            #  handle that yet, and possibly never)
             catalog = getToolByName(self.context, 'portal_catalog')
 
             persons = catalog({'object_provides': IPerson.__identifier__,
@@ -185,7 +226,7 @@ class GetMember(grok.View):
                 data['type'] = 'person'
                 data['personal_id'] = persons[0].personal_id
                 if show_details:
-                    data['name'] = persons[0].Title
+                    data['fullname'] = persons[0].Title
 
                 guardians = []
                 for person in persons:
@@ -209,6 +250,8 @@ class GetMember(grok.View):
                         query = {'object_provides': IRelation.__identifier__,
                                  'path': dict(query=person.getPath(),)}
 
+                        # TODO: What happens if en förening byter organisationsnummer
+                        # Bad things will happen... alla relationer med koppling till föreningen måste indexeras om
                         if vat_no:
                            query['personal_id'] = vat_no 
 
@@ -220,7 +263,9 @@ class GetMember(grok.View):
 
                         clubs = [dict(vat_no=club.personal_id, name=club.Title, uid=club.UID, status=club.review_state)
                                  for club in 
-                                 catalog(query) if club.personal_id]
+                                 catalog(query)]
+                                 # had earlier a condition where ther had to be a vat_no present
+                                 #  if club.personal_id
                         guardian['associated_clubs'] = clubs
 
                         # only include guardians where club is present
@@ -278,6 +323,51 @@ class GetMember(grok.View):
                 data['kollkoll'] = user.getProperty('kollkoll')
 
 
+        self.request.response.setHeader('Content-Type', 'application/json')
+        return json.dumps(data, indent=4)
+
+
+class GetInvitedMembers(grok.View):
+    """ Lists a specific member and their available relations.
+        The relations can be either direct or in-direct through any of
+        the member's associated person
+    """
+
+    grok.context(ISiteRoot)
+    grok.name('get-invited-members')
+    grok.require('hejasverige.ApiView')  # this is the security declaration
+
+    def render(self):
+        vat_no = self.request.form.get('vat_no', None)
+        data = []
+
+        if vat_no:
+            vat_no = vat_no.replace('-','')
+
+            catalog = getToolByName(self.context, 'portal_catalog')
+            clubs = catalog({'object_provides': IClub.__identifier__,
+                             'personal_id': vat_no,
+                              }
+                             )
+
+            from hejasverige.invitation.interfaces import IInvitation
+
+            for club in clubs:
+                query = {}
+                query['object_provides'] = IInvitation.__identifier__
+                query['path'] = dict(query=club.getPath())
+                invitations = catalog(query)
+
+                for invitation in invitations:
+                    o = invitation.getObject()
+                    rec = {}
+                    rec['type'] = 'invited'
+                    rec['personal_id'] = o.personal_id
+                    rec['fullname'] = o.first_name + ' ' + o.last_name
+                    rec['email'] = o.recipient_email
+                    rec['personal_id'] = o.personal_id
+                    rec['status'] = invitation.review_state
+                    data.append(rec)
         self.request.response.setHeader('Content-Type', 'application/json')
         return json.dumps(data, indent=4)
 
